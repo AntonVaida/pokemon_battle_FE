63f6655777acfa6bff37d5d2c3d3bfabc5754863
Anton Vaida <tohan1.ua@gmail.com>
2025-03-09 18:54:30 +0100

    fixed accessToken

--- app/pokemon/page.jsx
+++ app/pokemon/page.jsx
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export default async function SelectPokemon () {
   const cookieStore = await cookies();
-  const accessToken = cookieStore.get("accessToken")?.value || null;
+  const accessToken = cookieStore.get("accessToken");
 
   return (
    <PokemonPage accessToken={accessToken} />
